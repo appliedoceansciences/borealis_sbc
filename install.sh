bdb4fb36f3ee3149be112ec742cd8981545e1d15
Richard Campbell <richard.campbell@appliedoceansciences.com>
2025-03-05 18:13:16 -0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+apt update
+
+apt install socat gpsd chrony build-essential
+
+# installing gpsd enables this, we don't want it
+systemctl disable --now gpsd.socket
+
+# compile and install cobs_to_shm
+make -C cobs_to_shm
+mv cobs_to_shm/cobs_to_shm /usr/local/bin/
+make -C cobs_to_shm clean
+
+cp handle_sbc_command.py /usr/local/bin/
+
+cp *.service /etc/systemd/system/
+systemctl daemon-reload
+
+if ! grep SHM /etc/chrony/chrony.conf > /dev/null; then
+    printf 'refclock SHM 0 offset 0.0 delay 0.2\nrefclock SHM 1 offset 0.0 delay 0.0\n' >> /etc/chrony/chrony.conf
+fi
