6d730553ba351e2693614f2afc09e2322255ac96
Richard Campbell <richard.campbell@appliedoceansciences.com>
2025-03-16 13:58:36 -0700

    set system clock from GPZDA or GPRMC in handle_sbc_command prior to exec

--- handle_sbc_command.py
+++ handle_sbc_command.py
@@ -5,6 +5,51 @@ import sys
 import fcntl
 import os
 
+import time
+import calendar
+
+def handle_gpzda_or_gprmc(line):
+    if line[0] != '$': return False
+
+    try: payload, suffix = line[1:].split('*')
+    except: return False
+
+    checksum = 0
+    for byte in payload:
+        checksum ^= ord(byte)
+
+    if int(suffix, 16) != checksum:
+        print('nmea checksum error', file=sys.stderr)
+        return False
+
+    if 'ZDA' == payload[2:5]:
+        try:
+            tokens = payload.split(',')
+            hour = int(tokens[1][0:2])
+            minute = int(tokens[1][2:4])
+            sec = float(tokens[1][4:])
+            day = int(tokens[2])
+            month = int(tokens[3])
+            year = int(tokens[4])
+        except: return False
+
+    elif 'RMC' == payload[2:5]:
+        try:
+            tokens = payload.split(',')
+            hour = int(tokens[1][0:2])
+            minute = int(tokens[1][2:4])
+            sec = float(tokens[1][4:])
+            day = int(tokens[9][0:2])
+            month = int(tokens[9][2:4])
+            year = int(tokens[9][4:6]) + 2000
+        except: return False
+    else: return False
+
+    unixsec = calendar.timegm((year, month, day, hour, minute, sec))
+    print('setting time to', unixsec, file=sys.stderr)
+    time.clock_settime(time.CLOCK_REALTIME, unixsec)
+    return True
+
 def lock_uart_and_write_bytes(uart, bytes: bytes):
     fcntl.lockf(uart, fcntl.LOCK_EX)
     uart.write(bytes)
@@ -15,13 +60,17 @@ uart = serial.Serial(port=sys.argv[1], baudrate=115200, timeout=1)
 # send a cobs packet containing '?'
 lock_uart_and_write_bytes(uart, b"\x02\x3f\x00")
 
+handled_time = False
+sbc_command = None
+
 # loop until we get the expected response
-while True:
+while not handled_time or sbc_command is None:
     line = uart.readline().decode('utf-8')
 
     # if the above returns without a newline, it timed out, we should repeat the request
     if line == '':
-        lock_uart_and_write_bytes(uart, b"\x02\x3f\x00")
+        if sbc_command is None:
+            lock_uart_and_write_bytes(uart, b"\x02\x3f\x00")
         continue
 
     # strip newline and ignore empty lines
@@ -29,6 +78,10 @@ while True:
     if line == '': continue
 
     print(line, file=sys.stderr)
+
+    if handle_gpzda_or_gprmc(line):
+        handled_time = True
+        continue
 
     # check for and remove the expected prefix, ignoring other lines
     command = line.removeprefix('sbc_command: ')
@@ -38,10 +91,12 @@ while True:
 
     # TODO: validation of which commands can be run?
 
-    # close uart just in case the command results in something (gpsd) opening it
-    uart.close()
+    sbc_command = command
 
-    # replace this process with the command
-    args = command.split(' ')
-    os.execvp(args[0], args)
-    # not reached
+# close uart just in case the command results in something (gpsd) opening it
+uart.close()
+
+# replace this process with the command
+args = sbc_command.split(' ')
+os.execvp(args[0], args)
+# not reached
