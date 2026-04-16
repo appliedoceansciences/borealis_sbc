5b80cdce3b5ebf1221abf8c00c52c33ed257594f
Zachary Crockett <zachary.crockett@sofarocean.com>
2026-04-16 15:02:01 -0700

    feat!: mote-sbc communication for borealis v2
    - add bm_sbc_gateway.service and enables in install.sh
    - removes socat relay on uart, keeping gpsd listening on port 5000
    
    BREAKING CHANGE: removes handle_sbc_command

--- install.sh
+++ install.sh
@@ -13,12 +13,11 @@ make -C cobs_to_shm
 mv cobs_to_shm/cobs_to_shm /usr/local/bin/
 make -C cobs_to_shm clean
 
-cp handle_sbc_command.py /usr/local/bin/
 cp borealis_default.sh /usr/local/bin/
 
 cp *.service /etc/systemd/system/
 systemctl daemon-reload
-systemctl enable handle_sbc_command
+systemctl enable bm_sbc_gateway
 
 if ! grep SHM /etc/chrony/chrony.conf > /dev/null; then
     printf 'refclock SHM 0 offset 0.0 delay 0.2\nrefclock SHM 1 offset 0.0 delay 0.0\n' >> /etc/chrony/chrony.conf
